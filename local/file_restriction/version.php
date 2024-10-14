a14332bf287ba7f98a1b9d84ba09ed6f84a05687
Juani Villanueva <juani2002villa@icloud.com>
2024-10-14 13:41:18 -0300

    add icon to plugin

---
+++ local/file_restriction/version.php
@@ -0,0 +1,9 @@
+<?php
+// version.php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_file_restriction';
+$plugin->version = 2024101412;
+$plugin->release = '4.4';
+$plugin->icon = 'pix/icon.svg';
