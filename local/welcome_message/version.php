1f3ec26ae110f8abb8e5785edfe0bd7c51d022fb
Juani Villanueva <juani2002villa@icloud.com>
2024-10-14 14:43:24 -0300

    add icon to plugin

---
+++ local/welcome_message/version.php
@@ -0,0 +1,7 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_welcome_message';
+$plugin->version = 2024101412; // Fecha en formato AAAAMMDDHH
+$plugin->release = '4.4';
+$plugin->icon = 'pix/icon.svg';
