a83fba662b2380ad0ef8accb52333e80bfb9ab17
Juani Villanueva <juani2002villa@icloud.com>
2024-10-14 14:42:22 -0300

    add icon to plugin

---
+++ local/page_redirect/version.php
@@ -0,0 +1,7 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_page_redirect';
+$plugin->version = 2024101412;
+$plugin->release = '4.4';
+$plugin->icon = 'pix/icon.svg';
