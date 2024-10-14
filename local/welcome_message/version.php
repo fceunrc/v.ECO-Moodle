0994e618a57c05e3857e55ad4772baf91845d164
Juani Villanueva <juani2002villa@icloud.com>
2024-10-14 14:42:51 -0300

    Delete local/welcome_message/version.php

--- local/welcome_message/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->component = 'local_welcome_message';
-$plugin->version = 2024101213; // Fecha en formato AAAAMMDDHH
-$plugin->requires = 2020061500; // Versión mínima de Moodle (ejemplo para Moodle 3.9)
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0';
